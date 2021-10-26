cde9e4ae839eb3f7ef26bc7a2311adcfe5cfb2f8
lethanhquoc2014 <lethanhquoc.vnit@gmail.com>
2021-10-26 20:55:20 +0700

    mysql

---
+++ mysql/transaction/auto_commit.sql
@@ -0,0 +1,12 @@
+-- Active auto commit
+SET AUTOCOMMIT = 1
+SET AUTOCOMMIT = TRUE;
+
+-- Disable auto commit
+SET AUTOCOMMIT = 0;
+SET AUTOCOMMIT = FALSE;
+
+/*
+Mặc định thì autocommit  = 1, tức là tự động commit. Nhưng để test được transaction thì phải sét autocommit = 0;
+Do đó, khi insert, update data muốn thực hiện được thì phải sử dụng commit.
+*/
